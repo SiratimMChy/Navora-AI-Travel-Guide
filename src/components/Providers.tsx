8a9c7c68f38b4780cb4170adf6a0b97f77faeb9c
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2026-03-28 22:19:26 +0600

    Add components and providers

---
+++ src/components/Providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      {children}
+      <ToastContainer position="top-right" autoClose={3000} />
+    </SessionProvider>
+  );
+}
